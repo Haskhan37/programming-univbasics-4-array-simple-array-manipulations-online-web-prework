dcd9a0a442a228de84772a6fce2b4ed102797c98
Hasan Khan <hassankhankabacha@gmail.com>
2019-09-29 16:12:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,4 @@
-colors= ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_of_the_rainbow= ["red", "orange", "yellow", "green", "blue", "indigo"]
     next_color = "violet"
     colors.push(next_color)
+    
